a86e29e7f23ef9cb288fbc980c84072068fb735f
Suryansh Soni <surisoni10@gmail.com>
2023-12-16 22:34:24 +0530

    Updated solution

--- Assignments/Week2/Assignment1/01-async-js/hard (promises)/2-sleep-completely.js
+++ Assignments/Week2/Assignment1/01-async-js/hard (promises)/2-sleep-completely.js
@@ -7,12 +7,17 @@
 function sleep(milliseconds) {
   // Creating promise using the constructor.
   return new Promise((resolve, reject) => {
-    // Using setTimeout() function to create delay.
-    setTimeout(() => {
-      // After the delay is completed, the promise gets resolved.
-      resolve();
-      // Setting the timer in milliseconds.
-    }, milliseconds);
+    // Store the start time which will be used to measure the time passed.
+    const startTime = Date.now();
+    // Now to make the JS thread busy, it has to perform a heavy operation.
+    // The operation will be checking if the elapsed time is less than the given milliseconds.
+    // So, this loop will continuously run until the specified milliseconds have passed.
+    // During this time, the thread is occupied and is unable to perform other tasks.
+    while (Date.now() - startTime < milliseconds) {
+      // The loop is doing nothing. It is just to block the thread.
+    }
+    // Resolve the promise after the specified time.
+    resolve();
   });
 }
 
